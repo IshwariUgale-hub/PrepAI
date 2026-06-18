d747f4f7a3bef913b28809923eb1b00445b2452c
IshwariUgale-hub <ugaleishwari06@gmail.com>
2026-06-18 21:20:26 +0530

    Feat: Adding Auth Routes

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,16 @@
+//these are only routes that for user authentication means user can go on that routes...
+
+
+const express =require('express');
+const router = express.Router();
+const authcontroller = require('./controllers/auth.controller');
+
+router.post('/register', authcontroller.register);
+router.post('/login', authcontroller.login);
+router.post('/logout',authcontroller.logout );
+router.post('/forgot-password',authcontroller.forgotPassword );
+router.post('/reset-password', authcontroller.resetPassword );
+
+router.get('/verify/:token', authcontroller.verifyEmail);
+
+module.exports = router;
