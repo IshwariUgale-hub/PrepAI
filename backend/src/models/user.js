8cb0e2c74d4b66dbc5a2e1d1c3d442f0e99d0664
IshwariUgale-hub <ugaleishwari06@gmail.com>
2026-06-18 21:11:43 +0530

    Feat:adding the user schema for the mongodb

---
+++ backend/src/models/user.js
@@ -0,0 +1,20 @@
+//user schema means the information user need for auth.
+
+const mongoose  =require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name:{type:String ,required: true},
+    email: {type:String  , required:true ,unique:true},
+    password: {type:String , required:true},
+    password_confirmation: {type:String , required:true},
+    avatar: {type:String},
+    role:{type:String , enum:['user','admin'],default:'user'},
+    isVerified: {type:Boolean , default: false},
+    subscription: {type:String, enum:['free','pro'], default:'free'},
+    verifyToken:{type:String},
+    resetToken:{type:String},
+
+
+}, {timestamps:true});
+
+module.exports = mongoose.model('user',userSchema);
